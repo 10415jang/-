d960507b2ad61445a755c8299c1f6fa292609513
jang <janghisang@gmail.com>
2025-08-09 11:15:15 +0900

    첫번째 파이썬 파일 생성

---
+++ verable.py
@@ -0,0 +1,22 @@
+x = "안녕"
+
+print(x)
+
+x = 3
+print(x)
+
+x = 3 + 5
+print(x)
+
+x = 3 == 5
+print(x)
+
+x = True
+print(x)
+
+x = [1,2,3,4,5,5,6,6,66,7] #list
+print(x)
+
+x = {'a' : 1, 'b' :5} #dictionary key : value
+print(x)
+print(x['a'])
