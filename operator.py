c34652c80f759b8f6683e822e45f0b5709d1b2fe
jang <janghisang@gmail.com>
2025-08-30 09:43:01 +0900

    문자 형태의 연산

--- operator.py
+++ operator.py
@@ -32,3 +32,13 @@ print("x//y",z)
 
 z=x**y
 print("x**y",z)
+
+str_x="hello"
+str_y="python"
+
+str_z=str_x+str_y
+print("str_x+str_y:",str_z)
+
+# unsupported operand type(s) for -: 'str' and 'str'
+# str_z = str_x - str_y
+# print("str_x + str_y:", str_z)
