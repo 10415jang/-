be0f277467c55ea13f585c0e93f4fd28d19d5f74
jang <janghisang@gmail.com>
2025-11-22 12:09:05 +0900

    문제 제출

--- control. statement.py
+++ control. statement.py
@@ -1,53 +1,55 @@
-def print_ usingif():
+def print_score():
     score = int(input("점수를 입력하세요."))
 
-if score > 100 or score < 0:
-    print("정상적인 점수 범위가 아닙니다")
-else:
-    if score >=80:
-        print(" 합격입니다.")
-    elif score >=60:
-         print("재시험 응시가 필요합니다.")
+    if score > 100 or score < 0:
+        print("정상적인 점수 범위가 아닙니다")
     else:
-         print("불합격입니다")
+        if score >=80:
+            print(" 합격입니다.")
+        elif score >=60:
+            print("재시험 응시가 필요합니다.")
+        else:
+            print("불합격입니다")
 
-        if score <= 100:
-            if score >= 0:
+while True:
+    user_input = input("값을 입력하시오 :")
 
-# while True:
-#         user_input = (input("같은 값을 입력하세요 :"))
-#
-#         if user_input.lower() == "z":
-#             break
-input_number = int(input("숫자를 입력하세요"))
-index = 1
+    if user_input.lower()=="z":
+        break
 
-print("짝수 출력 ---->")
-while index <= input_number:
-    if index % 2 == 0:
-      print(index)
-    index = index + 1
+# 짝수만 출력
+def print_even():
+    input_number = int(input("숫자를 입력하세요."))
+    index = 1
+    while index <= input_number:
+        if index % 2 == 0:
+            print(index)
+        index = index + 1
+
 
 #피보나치 수열
-def print_pibonacci_type():
-list = [1,1]
+def print_pibonacci_with_list():
+    input_number = int(input("숫자를 입력하세요."))
+    index = 1
+    list = [1,1]
 
-while list[-1] <= input_number:
-    print(list[-1])
-    list.append(list[-1] + list[-2])
+    while index <= input_number:
+        print(list[-1])
+        list.append(list[-1] + list[-2])
 
 
 print("피보나치 수열 without list")
 def print_fibonacci_with_variables():
-a = 1
-b = 1
-c = 1
+    input_number = int(input("숫자를 입력하세요."))
+    a = 1
+    b = 1
+    c = 1
 
-while c<= input_number:
-    print(c)
-    c = a + b
-    a=b
-    b=c
+    while c<= input_number:
+        print(c)
+        c = a + b
+        a=b
+        b=c
 
 def repeat_function():
     i=1
@@ -66,21 +68,82 @@ def repeat_function():
 #
 
 #수학
-test = [{ 'answer':[1, 3, 2, 4, 5, 3, 1, 2, 3, 4]},
- { 'answer':[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]},
- { 'answer':[1, 5, 3, 4, 5, 1, 2, 3, 1, 2]},
- { 'answer':[4, 3, 4, 4, 5, 3, 1, 2, 2, 4]},
- { 'answer':[1, 3, 4, 4, 5, 3, 1, 2, 3, 2]},
- { 'answer':[1, 3, 5, 4, 5, 3, 1, 2, 3, 4]},]
+test = [
+    {
+        'name': 'aaa',
+        'number': 10101,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'bbb',
+        'number': 10102,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'ccc',
+        'number': 10103,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'ddd',
+        'number': 10104,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'eee',
+        'number': 10105,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    }
+]
+correct_answer = {
+    'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+}
 
-a = [1, 3, 2, 4, 5, 3, 1, 2, 3, 4]
-correct_answer = [1, 3, 2, 4, 5, 3, 1, 2, 3, 4]
+def get_score(student_answer, correct_answer):
+    score = 0
+    for (student, correct) in zip(student_answer, correct_answer):
+        if student == correct:
+            score= score+10
+    return score
 
-#학생의 점수구하기 한 문항당 10점이라 가정
-score = 0
-for (student, correct) in zip(a, correct_answer):
-    print(student, '/', correct)
-    if student == correct:
-        score= score+10
-print(score)
+for student in test:
+
+    correct_answer = {
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    }
+    for student in test:
+        if student['name']== 'ccc': #배열 반복에서 스킵하는 방법
+            continue
+
+        print("학생", student['name'],"===========")
+        math_score = get_score(student['math'], correct_answer['math'])
+        korean_score = get_score(student['korean'], correct_answer['korean'])
+        english_score = get_score(student['english'], correct_answer['english'])
+        science_score = get_score(student['science'], correct_answer['science'])
+
+        print("수학점수:", math_score)
+        print("국어점수:", korean_score)
+        print("영어점수:", english_score)
+        print("과학점수:", science_score)
 
